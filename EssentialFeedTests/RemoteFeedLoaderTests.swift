35663abc133e5ed563ec091274f8023831dddc94
Ashish Katiyar <ashish.katiyar@paytm.com>
2020-12-10 23:47:51 +0530

    test:- does not request data from url

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -12,11 +12,16 @@ class RemoteFeedLoader {
     
 }
 
+class HTTPClient {
+    var requestedURL: URL?
+}
+
 class RemoteFeedLoaderTests: XCTestCase {
 
-    func test_init() {
-        let sut = RemoteFeedLoader()
+    func test_init_doesNotRequestDataFromURL() {
+        _ = RemoteFeedLoader()
+        let client = HTTPClient()
         
-        
+        XCTAssertNil(client.requestedURL)
     }
 }
