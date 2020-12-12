257a21e14c710948eb5f7637f09536fdec63b220
Ashish Katiyar <ashish.katiyar@paytm.com>
2020-12-12 12:16:44 +0530

    delivers connectivity error on client error

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -9,19 +9,25 @@
 import Foundation
 
 public protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Error)->Void)
 }
 
 public class RemoteFeedLoader {
     private let client: HTTPClient
     private let url: URL
     
+    public enum Error: Swift.Error {
+        case connectivity
+    }
+    
     public init(url: URL = URL(string: "http://a-URL.com")!, client: HTTPClient) {
         self.client = client
         self.url = url
     }
     
-    public func load() {
-        client.get(from : url)
+    public func load(completion: @escaping (Error)->Void = {_ in}) {
+        client.get(from : url) {_ in
+            completion(.connectivity)
+        }
     }
 }
